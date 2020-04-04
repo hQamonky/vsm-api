a189cfec947d4241c3a36b5a618e48385f1cfba2
qmk <wherlicq@gmail.com>
2020-04-04 11:48:40 +0200

    Added bash command but doesn't work.

--- endpoints/__init__.py
+++ endpoints/__init__.py
@@ -2,7 +2,7 @@ import markdown
 import os
 
 # Import the framework
-from flask import Flask
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
@@ -16,3 +16,23 @@ def index():
         content = markdown_file.read()
         # Convert to HTML
         return markdown.markdown(content)
+
+
+@app.route("/sinks", methods=["GET"])
+def get_sinks():
+    import subprocess
+    process = subprocess.run(["pactl", "list", "short", "sinks"], check=True, stdout=subprocess.PIPE,
+                             universal_newlines=True)
+    data = []
+    sinks = process.stdout.split('\n')
+    for sink in sinks:
+        sink_array = sink.split('\t')
+        if len(sink_array) == 5:
+            data.append({
+                'id': sink_array[0],
+                'name': sink_array[1],
+                'driver': sink_array[2],
+                'sample_specification': sink_array[3],
+                'state': sink_array[4]
+            })
+    return jsonify(data)
